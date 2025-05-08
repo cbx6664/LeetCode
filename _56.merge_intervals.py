9cdcc2db4b7d3a35af0fada1b8a0f2f881893171
CBX <chenbingxu369@gmail.com>
2025-05-08 11:28:12 +0800

    1

---
+++ _56.merge_intervals.py
@@ -0,0 +1,16 @@
+from typing import List
+import pytest
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda p: p[0])  # 按照左端点从小到大排序
+        ans = []
+        for p in intervals:
+            if ans and p[0] <= ans[-1][1]:  # 可以合并
+                ans[-1][1] = max(ans[-1][1], p[1])  # 更新右端点最大值
+            else:  # 不相交，无法合并
+                ans.append(p)  # 新的合并区间
+        return ans
+
+solution = Solution()
+print(solution.merge([[1,3],[2,6],[8,10],[15,18]]))
